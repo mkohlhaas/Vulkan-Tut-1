e78467c62d5afa451f1ca80952d004b0231bedb5
Hans-J. Schmid <knock@myopendoor.de>
2024-03-16 18:12:09 +0100

    recording

--- src/cmdPool.c
+++ src/cmdPool.c
@@ -13,18 +13,15 @@ void createCommandPool() {
       .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
       .queueFamilyIndex = queueFamilyIndex,
   };
-
   EH(vkCreateCommandPool(device, &createInfo, nullptr, &cmdPool));
 }
 
 void allocateCommandBuffer() {
-
   VkCommandBufferAllocateInfo allocateInfo = {
       .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
       .commandPool = cmdPool,
       .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
       .commandBufferCount = 1,
   };
-
   EH(vkAllocateCommandBuffers(device, &allocateInfo, &cmdBuffer));
 }
---
+++ src/recording.c
@@ -0,0 +1,62 @@
+#include "cmdPool.h"
+#include "error.h"
+#include "framebuffer.h"
+#include "pipeline.h"
+#include "renderpass.h"
+#include "swapchain.h"
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
+
+static void beginCmdBuffer() {
+  VkCommandBufferBeginInfo commandBufferBeginInfo = {
+      .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+      .flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT,
+  };
+  EH(vkBeginCommandBuffer(cmdBuffer, &commandBufferBeginInfo));
+}
+
+static void beginRenderPass(uint32_t imageIndex) {
+  // renderPass → colorAttachmentDescription → loadOp
+  const VkClearColorValue colorClearValue = {0.0f, 0.0f, 0.0f, 1.0f}; // black
+  const VkClearValue clearValues[] = {colorClearValue};
+
+  VkRenderPassBeginInfo renderPassBeginInfo = {
+      .sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+      .renderPass = renderPass,
+      .framebuffer = swapchainFramebuffers[imageIndex],
+      .renderArea = {{0, 0}, swapchainExtent},
+      .clearValueCount = sizeof(clearValues) / sizeof(VkClearColorValue),
+      .pClearValues = clearValues,
+  };
+  vkCmdBeginRenderPass(cmdBuffer, &renderPassBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
+}
+
+static void bindPipeline() { vkCmdBindPipeline(cmdBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline); }
+
+// Dynamic States in pipeline
+static void setViewport() {
+  VkViewport viewport = {0.0f, 0.0f, swapchainExtent.width, swapchainExtent.height, 0.0f, 1.0f};
+  vkCmdSetViewport(cmdBuffer, 0, 1, &viewport);
+}
+
+static void setScissor() {
+  VkRect2D scissor = {{0.0f, 0.0f}, swapchainExtent};
+  vkCmdSetScissor(cmdBuffer, 0, 1, &scissor);
+}
+
+static void draw() { vkCmdDraw(cmdBuffer, 3, 1, 0, 0); }
+
+static void endRenderPass() { vkCmdEndRenderPass(cmdBuffer); }
+
+static void endCmdBuffer() { EH(vkEndCommandBuffer(cmdBuffer)); }
+
+void recordCmdBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex) {
+  beginCmdBuffer();
+  beginRenderPass(imageIndex);
+  bindPipeline();
+  setViewport();
+  setScissor();
+  draw();
+  endRenderPass();
+  endCmdBuffer();
+}
